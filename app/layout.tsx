fe3eee31e23dba69e23ac4b0ff82af37435661bd
David Chukwudi Onyeka (Nokia) <david.chukwudi_onyeka@nokia.com>
2025-09-01 11:29:39 +0100

    dark-theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <ThemeInitializer />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
