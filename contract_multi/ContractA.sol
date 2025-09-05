71969a7675c35d3be8626256d3e59d53d21f868a
akshayacodified <akshaya071999@gmail.com>
2025-09-05 17:42:24 +0530

    removed unnecessary file

--- contract_multi/ContractA.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract ContractA {
-    function getA() public pure returns (string memory) {
-        return "This is Contract A";
-    }
-}
